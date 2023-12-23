9fc368b41449869465b5a1485ffb94046eafde5a
Carol Pinheiro <caroline@bossabox.com>
2023-12-23 19:31:07 -0300

    feat: update swagger title

--- src/api/docs/index.ts
+++ src/api/docs/index.ts
@@ -3,7 +3,7 @@ import { ApiPaths } from "./paths";
 export const SwaggerConfig = {
     openapi: "3.0.0",
     info: {
-        title: "fiap-soat1-tech-challenge-produto-app",
+        title: "Rotas de Produto - Fiap Arquitetura de Software",
         description:
             "Projeto do curso da pós tech Fiap Arquitetura de Software",
         version: "1.0.0",
@@ -17,18 +17,6 @@ export const SwaggerConfig = {
         {
             name: "produto",
             description: "Rotas relacionadas a produto",
-        },
-        {
-            name: "cliente",
-            description: "Rotas relacionadas a cliente",
-        },
-        {
-            name: "pedido",
-            description: "Rotas relacionadas a pedido",
-        },
-        {
-            name: "webhook",
-            description: "Rotas relacionadas a webhooks",
         },
     ],
     paths: ApiPaths,
