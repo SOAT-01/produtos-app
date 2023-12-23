8b0978ecadbbc61081334370932b84f401b86782
Carol Pinheiro <caroline@bossabox.com>
2023-12-23 19:18:22 -0300

    feat: add produtos code migration to repo

---
+++ src/api/docs/index.ts
@@ -0,0 +1,35 @@
+import { ApiPaths } from "./paths";
+
+export const SwaggerConfig = {
+    openapi: "3.0.0",
+    info: {
+        title: "fiap-soat1-tech-challenge-produto-app",
+        description:
+            "Projeto do curso da pós tech Fiap Arquitetura de Software",
+        version: "1.0.0",
+    },
+    servers: [
+        {
+            url: "/api",
+        },
+    ],
+    tags: [
+        {
+            name: "produto",
+            description: "Rotas relacionadas a produto",
+        },
+        {
+            name: "cliente",
+            description: "Rotas relacionadas a cliente",
+        },
+        {
+            name: "pedido",
+            description: "Rotas relacionadas a pedido",
+        },
+        {
+            name: "webhook",
+            description: "Rotas relacionadas a webhooks",
+        },
+    ],
+    paths: ApiPaths,
+};
