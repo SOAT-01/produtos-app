e0db74f4ad81b39b1952731228ecb3d690d7e4db
Carol Pinheiro <caroline@bossabox.com>
2024-01-06 11:15:56 -0300

    fix: update migration base

--- src/external/postgres/migrations/0000_rich_roland_deschain.sql
+++ src/external/postgres/migrations/0000_rich_roland_deschain.sql
@@ -1,13 +1,6 @@
-CREATE TABLE IF NOT EXISTS "clientes" (
-	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
-	"nome" varchar(256),
-	"email" varchar(256),
-	"cpf" varchar(256),
-	"deleted" boolean DEFAULT false,
-	"created_at" timestamp DEFAULT now() NOT NULL
-);
+CREATE SCHEMA "produto";
 --> statement-breakpoint
-CREATE TABLE IF NOT EXISTS "produtos" (
+CREATE TABLE IF NOT EXISTS "produto"."produtos" (
 	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
 	"name" varchar(256) NOT NULL,
 	"preco" numeric(10, 2) NOT NULL,
