3a597f4aaa3b91c9b545425957fdbc7f0f629692
Karine Riquena Limp <karine_limp@hotmail.com>
2024-03-03 23:05:54 -0300

    chore: corrige alertas apontados no OWASP ZAP

--- src/api/server.ts
+++ src/api/server.ts
@@ -1,14 +1,28 @@
 import express from "express";
+import helmet from "helmet";
+import nocache from "nocache";
 import { serve, setup } from "swagger-ui-express";
 import { errorHandler } from "./middlewares";
 import { makeServerRouter } from "./routes";
 import { requestLogger } from "../utils/requestLogger";
 import { SwaggerConfig } from "./docs";
+import { serverConfig } from "config";
 
 require("dotenv").config();
 
 function buildServer() {
     const server = express();
+
+    server.disable("x-powered-by");
+    server.use(helmet());
+    server.use(nocache());
+
+    if (serverConfig.isProduction) {
+        server.use(helmet.hsts({
+            maxAge: 31536000,
+            includeSubDomains: true
+        }));
+    }
 
     server.use(requestLogger);
 
